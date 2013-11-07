1026654c976fac701d092dd5b0600b8f5f9f4129
Takashi Masuda <masutaka.net@gmail.com>
2013-11-07 17:34:25 +0900

    Support inputting any files to fgrep.c

--- c/fgrep.c
+++ c/fgrep.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 #define TRUE  1
 #define FALSE 0
@@ -16,26 +17,53 @@ typedef unsigned long int       u32;
 
 typedef unsigned long int       bool;
 
-static void fgrep(const char *pattern, const char *filename);
+typedef void (line_printf_f)(const char *filename, const char *format, ...);
+
+typedef struct
+{
+    line_printf_f *line_printf;
+} fgrep_info_t;
+
+static void* initialize(int argc, char **argv);
+static void fgrep(void *instance, const char *pattern, const char *filename);
 static size_t get_file_size(FILE *fp);
-static void fgrep_buffer(const char *pattern, char *lines);
+static void fgrep_buffer(void *instance, const char *pattern, char *lines, const char *filename);
+static void line_printf_with_filename(const char *filename, const char *format, ...);
+static void line_printf_without_filename(const char *filename, const char *format, ...);
 
 int
 main(int argc, char **argv)
 {
-    char *pattern = argv[1];
-    char *filename = argv[2];
+    void *instance;
+    char *pattern;
+    int i;
 
-    fgrep(pattern, filename);
+    pattern = argv[1];
+
+    instance = initialize(argc, argv);
+
+    for(i = 2; i < argc; i++) {
+        fgrep(instance, pattern, argv[i]);
+    }
 
     return 0;
+}
+
+static void*
+initialize(int argc, char **argv)
+{
+    static fgrep_info_t fgrep_info;
+
+    fgrep_info.line_printf = (argc >= 4) ? line_printf_with_filename : line_printf_without_filename;
+
+    return &fgrep_info;
 #ifdef NOWARNING
-    (void)argc;
+    (void)argv;
 #endif  /* NOWARNING */
 }
 
 static void
-fgrep(const char *pattern, const char *filename)
+fgrep(void *instance, const char *pattern, const char *filename)
 {
     FILE *fp;
     size_t file_size;
@@ -60,7 +88,7 @@ fgrep(const char *pattern, const char *filename)
         goto ENDPROC1;
     }
 
-    fgrep_buffer(pattern, lines);
+    fgrep_buffer(instance, pattern, lines, filename);
 
 ENDPROC1:
     free(lines);
@@ -84,8 +112,10 @@ get_file_size(FILE *fp)
 }
 
 static void
-fgrep_buffer(const char *pattern, char *lines)
+fgrep_buffer(void *instance, const char *pattern, char *lines, const char *filename)
 {
+    fgrep_info_t *fgrep_infop = instance;
+    line_printf_f *line_printf = fgrep_infop->line_printf;
     char *line;
 
     if((line = strtok(lines, "\n")) == '\0') {
@@ -93,14 +123,44 @@ fgrep_buffer(const char *pattern, char *lines)
     }
 
     if(strstr(line, pattern) != NULL) {
-        printf("%s\n", line);
+        line_printf(filename, "%s\n", line);
     }
 
     while((line = strtok(NULL, "\n")) != '\0') {
         if(strstr(line, pattern) != NULL) {
-            printf("%s\n", line);
+            line_printf(filename, "%s\n", line);
         }
     }
 
     return;
+}
+
+static void
+line_printf_with_filename(const char *filename, const char *format, ...)
+{
+    va_list arg;
+    char format_with_filename[1024];
+
+    sprintf(format_with_filename, "%s: %s", filename, format);
+
+    va_start(arg, format);
+    vprintf(format_with_filename, arg);
+    va_end(arg);
+
+    return;
+}
+
+static void
+line_printf_without_filename(const char *filename, const char *format, ...)
+{
+    va_list arg;
+
+    va_start(arg, format);
+    vprintf(format, arg);
+    va_end(arg);
+
+    return;
+#ifdef NOWARNING
+    (void)filename;
+#endif  /* NOWARNING */
 }
